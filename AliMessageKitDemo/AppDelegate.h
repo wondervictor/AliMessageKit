ab6d2f0ea21e22b52bdd70e9026d8aed318aff16
VicChan <victorchanchina@gmail.com>
2016-09-03 20:28:27 +0800

    Initial Commit

---
+++ AliMessageKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AliMessageKitDemo
+//
+//  Created by VicChan on 16/9/3.
+//  Copyright © 2016年 VicChan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AliMessageKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AliMessageKitDemo
+//
+//  Created by VicChan on 16/9/3.
+//  Copyright © 2016年 VicChan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
